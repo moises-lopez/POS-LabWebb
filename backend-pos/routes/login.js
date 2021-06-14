87a68210627630d33e8dc82860d38f8c69b74a8c
unknown <you@example.com>
2021-06-13 19:24:48 -0500

    commit to heroku

--- backend-pos/routes/login.js
+++ backend-pos/routes/login.js
@@ -4,49 +4,60 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
-
 router.post("/login", async function (req, res) {
   let query = {
-      email: req.body.email,
-      contrasenia: req.body.contrasenia,
-    };
+    email: req.body.email,
+    contrasenia: req.body.contrasenia,
+  };
 
+  const usuario = await Usuarios.find({
+    email: req.body.email,
+  });
 
-    const usuario = await Usuarios.find(
-      {
-        email:req.body.email
-      }
-    );
+  if (usuario.length == 0) {
+    return res.status(400).send("Email no existe");
+  }
 
+  const validPass = await bcrypt.compare(
+    req.body.contrasenia,
+    usuario[0].contrasenia
+  );
 
-    if (usuario.length == 0) {
-      return res.status(400).send("Email no existe");
-    }
+  if (!validPass) {
+    return res.status(400).send("Contraseña incorrecta");
+  }
+  const token = jwt.sign({ email: usuario[0].email }, process.env.TOKEN_SECRET);
 
-    const validPass = await bcrypt.compare(
-      req.body.contrasenia,
-      usuario[0].contrasenia
-    );
-
-    if (!validPass) {
-      return res.status(400).send("Contraseña incorrecta");
-    }
-    const token = jwt.sign({ email: usuario[0].email }, process.env.TOKEN_SECRET);
-
-    res.send(token);
+  res.send(token);
 });
 
-
 router.post("/registro", async function (req, res) {
-    const salt = await bcrypt.genSalt(10);
-    const hashcontrasenia = await bcrypt.hash(req.body.contrasenia, salt);
-    let usuario = {
-      email: req.body.email,
-      contrasenia: hashcontrasenia,
-    };
-    const newUsuario = new Usuarios(usuario);
-    await newUsuario.save(usuario);
+  const salt = await bcrypt.genSalt(10);
+  const hashcontrasenia = await bcrypt.hash(req.body.contrasenia, salt);
+  let usuario = {
+    email: req.body.email,
+    contrasenia: hashcontrasenia,
+  };
+  const newUsuario = new Usuarios(usuario);
+  await newUsuario.save(usuario);
+});
+
+router.get("/autorizacion", async (req, res) => {
+  console.log(
+    "auth///////////////////////////////////////////////",
+    req.headers.token
+  );
+  const token = req.headers.token;
+  if (token == null) {
+    res.status(400).send("FALSE");
+  }
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    res.send(verified);
+  } catch (err) {
+    res.status(400).send("FALSE");
+  }
 });
 
 module.exports = router;
--- backend-pos/routes/sales.js
+++ backend-pos/routes/sales.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const moment = require("moment");
+moment().format();
+
 const Sale = require("../models/sales.model");
 
 const idFilter = (req) => (list) => list._id === parseInt(req.params.id);
@@ -15,14 +18,28 @@ router.get("/", (req, res) => {
       console.log("error: Something broke");
     });
 });
+const getVentasSemanal = async (week) => {
+  let start = moment().subtract(week, "week").startOf("day");
+  let end = moment().subtract(week, "week").endOf("day");
+  const ventasSemanal = [];
+  for (let index = 0; index < 7; index++) {
+    console.log(start);
+    const ventaDia = await aggregateVentasHoy(start.toDate(), end.toDate());
+    ventasSemanal.push({
+      fecha: start.toDate().toLocaleDateString("en-US", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      }),
+      ventas: ventaDia,
+    });
+    start = start.subtract(1, "day");
+    end = end.subtract(1, "day");
+  }
+  return ventasSemanal;
+};
 
-const aggregateVentasHoy = async () => {
-  var start = new Date();
-  start.setHours(0, 0, 0, 0);
-
-  var end = new Date();
-  end.setHours(23, 59, 59, 999);
-
+const aggregateVentasHoy = async (start, end) => {
   const agg = await Sale.aggregate([
     { $match: { createdAt: { $gte: start, $lte: end } } },
     {
@@ -34,10 +51,28 @@ const aggregateVentasHoy = async () => {
       },
     },
   ]);
-  return agg[0]["Total sales"];
+  if (agg.length > 0) {
+    return agg[0]["Total sales"];
+  } else {
+    return 0;
+  }
 };
 
+router.get("/salesWeekly", async (req, res) => {
+  res.json(await getVentasSemanal(req.query.currentWeek));
+
+  // Sale.find({})
+  //   .then((data) => {
+  //     console.log("Sale: ", data);
+  //     res.json(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log("error: Something broke");
+  //   });
+});
+
 router.get("/salesToday", async (req, res) => {
+  console.log("HLO");
   res.json(await aggregateVentasHoy());
 
   // Sale.find({})
